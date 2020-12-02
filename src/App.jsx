54999e1daa7dd25cf9760faa4b61be86fdd4698f
lucas singh <lucas.singh10@gmail.com>
2020-12-02 14:23:23 -0300

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.scss';
+import Navbar from './components/shared/navbar/Navbar';
 
 function App() {
   return (
+
+    <>
+        <Navbar/>
+          
     <div className="App">
         <h1>Coder-shop</h1>
         <p>Proyecto e-commerce para curso de react Coderhouse</p>
     </div>
+    </>
   );
 }
 
---
+++ src/components/shared/navbar/NavbarData.jsx
@@ -0,0 +1,28 @@
+
+export const NavbarData = [
+    {
+        title: 'Home',
+        url: '/',
+        cname: 'nav-links'
+    },
+    {
+        title: 'Adidas',
+        url: '/adidas',
+        cname: 'nav-links'
+    },
+    {
+        title: 'Nike',
+        url: '/nike',
+        cname: 'nav-links'
+    },
+    {
+        title: 'Puma',
+        url: '/puma',
+        cname: 'nav-links'
+    },
+    {
+        title: 'Shop',
+        url: '/shop',
+        cname: 'nav-links-shop'
+    }
+]
