53ed74bdbd504d7362dfe32252409d1dd1c4a176
lucas singh <lucas.singh10@gmail.com>
2020-12-03 21:57:01 -0300

    img banner section and styles

---
+++ src/components/shared/banner/Banner-data.jsx
@@ -0,0 +1,17 @@
+export const BannerData = [
+    {
+        // title: 'Adidas PulseBoost',
+        urlImg: 'assets/banners/adidas-banner-1.jpg',
+        // textInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    },
+    {
+        // title: 'Adidas Run 90s',
+        urlImg: 'assets/banners/nike-banner-2.jpg',
+        // textInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    },
+    {
+        // title: 'Adidas Ultraboost',
+        urlImg: 'assets/banners/puma-banner-1.jpg',
+        // textInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    }
+]
