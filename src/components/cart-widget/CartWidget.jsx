408ae2e963f9fe3c28596e96a8da3679cb192139
lucas singh <lucas.singh10@gmail.com>
2020-12-05 00:33:46 -0300

    cambios widget

--- src/components/cart-widget/CartWidget.jsx
+++ src/components/cart-widget/CartWidget.jsx
@@ -9,16 +9,16 @@ function CartWidget() {
                     <div className="wraper">
                         <div className="cart-widget-product">
                             <h4 className="title-widget">Added to your cart</h4>
-                            <img src="assets/cards/adidas-3.png" alt=""/>
-                            <span className="text-detail-widget"> Snickers Adidas Booster Rapix</span>
+                            <img className="img-widget"src="assets/cards/adidas-3.png" alt=""/>
+                            <span className="text-detail-widget">Adidas Booster Rapix</span>
                             <span className="price-widget">U$d 125.00</span>
                         </div>
-                        <div className="cart-widget-product">
+                        <div className="cart-widget-product center-desktop">
                             <h4 className="title-widget">2 items added to your cart</h4>                        
                             <span className="text-detail-widget">Total cart:</span>
                             <span className="price-widget">U$d 275.00</span>
                         </div>
-                        <div className="cart-widget-product">
+                        <div className="cart-widget-product buttons centered">
                             <button className="button-secondary">View Cart</button>
                             <button className="button-primary">Shop</button>
                         </div>
