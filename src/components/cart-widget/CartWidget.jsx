e4954d8cf85b01f180ffbcd4ddf32886bb42ef6d
lucas singh <lucas.singh10@gmail.com>
2020-12-04 23:24:27 -0300

    cambios widget

--- src/components/cart-widget/CartWidget.jsx
+++ src/components/cart-widget/CartWidget.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
-//import { CartWidgetData } from './';
+import React from 'react'
 
 function CartWidget() {
-    const [sideBar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sideBar)
+   
     return (
         <>
-            
-        <div className={sideBar ? 'nav-menu active' : 'nav-menu'}>
-            <div className="cart-widget-container" onClick={showSidebar}>
-               widgetCart
+            <div >
+                <div className="cart-widget-container" >
+                    <div className="wraper">
+                        <div className="cart-widget-product">
+                            <h4 className="title-widget">Added to your cart</h4>
+                            <img src="assets/cards/adidas-3.png" alt=""/>
+                            <span className="text-detail-widget"> Snickers Adidas Booster Rapix</span>
+                            <span className="price-widget">U$d 125.00</span>
+                        </div>
+                        <div className="cart-widget-product">
+                            <h4 className="title-widget">2 items added to your cart</h4>                        
+                            <span className="text-detail-widget">Total cart:</span>
+                            <span className="price-widget">U$d 275.00</span>
+                        </div>
+                        <div className="cart-widget-product">
+                            <button className="button-secondary">View Cart</button>
+                            <button className="button-primary">Shop</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         </>
     );
 }
