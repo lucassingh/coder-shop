83a5905b24ef2ddbd95901caa0ec387c14997e11
lucas singh <lucas.singh10@gmail.com>
2020-12-20 13:23:41 -0300

    cambios

---
+++ src/components/products/product-detail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductDetail from './product-detail/ProductDetail';
+
+
+const ProductDetail = () => {
+   
+    return (
+        <>
+           <ProductDetail />
+        </>
+    );
+}
+export default ProductDetail;
